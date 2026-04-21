9b7bb7b8f794b64abef5088a1ec9caeb1a1ca0b8
kaufrickpro <karataskeremalp@gmail.com>
2026-04-21 12:04:11 +0300

    Fix repo structure for Render build

---
+++ part2-notes-frontend/src/services/notes.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+const baseUrl = import.meta.env.VITE_API_BASE_URL || '/api/notes'
+
+function getAll() {
+  const request = axios.get(baseUrl)
+  return request.then((response) => response.data)
+}
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then((response) => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then((response) => response.data)
+}
+
+export default {
+  getAll,
+  create,
+  update,
+}
